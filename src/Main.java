8c13ea3cf55a965a2d9d0a161f26ec05d8c8c254
Alex <sasikbaloun@gmail.com>
2026-09-13 19:13:14 +0200

    prvni-commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Popis toho co jsem dělal o prázdninách. Bylo to už poměrně dávno, asi tak dva týdny zpátky takže si to už moc nepamatuji. Vím že jsem byl v Chorvatsku. Taky jsem byl na brigádě a občas hrál fotbal. To je asi vše, zbytek času jsem byl prostě doma."));
+    }
+
